d0d7f4e7ed12de63a857e975fa98b036cbd60bad
Gustavo Oliveira Souza <guga.oli.1357@gmail.com>
2022-03-30 23:04:33 -0300

    Little changes

--- JavaScript/assets/script.js
+++ JavaScript/assets/script.js
@@ -1,3 +1,12 @@
 function callAlert() {
   alert('Olá, mundo!');
 }
+
+function doSomeCalc() {
+  let txt1 = document.querySelector('input1');
+  let txt2 = document.querySelector('input2');
+
+  if(!txt1)
+
+}
+
